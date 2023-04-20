6c12b95c58a5938146015bb80d48f6c3486b8c56
Clement t <ctorti@edevice.com>
2023-04-20 16:59:00 +0200

    Step route functional

--- backend/routes/stepRoutes.js
+++ backend/routes/stepRoutes.js
@@ -10,6 +10,13 @@ router.get('/steps', (req, res) => {
     .catch((error) => Logger.http(error))
 })
 
+router.get('/steps/:stepId', (req, res) => {
+  Step.findOne({ _id: req.params.stepId })
+    .populate('params')
+    .then((step) => res.send(step))
+    .catch((error) => Logger.http(error))
+})
+
 router.post('/steps', async (req, res) => {
   try {
     // Create and save the Param objects
@@ -37,6 +44,79 @@ router.post('/steps', async (req, res) => {
     Logger.http(err)
     res.status(500).json({ error: 'Internal server error' })
   }
+})
+
+// update a step
+router.put('/steps/:stepId', async (req, res) => {
+  try {
+    // Get the step
+    const step = await Step.findOne({ _id: req.params.stepId })
+
+    // Update the step
+    step.title = req.body.title
+    step.description = req.body.description
+    step.code = req.body.code
+    step.stepType = req.body.stepType
+
+    // Add the new params
+    for (const param of req.body.params.filter(param => !param._id)) {
+      const newParam = new Param(param)
+      await newParam.save()
+      step.params.push(newParam._id)
+      param._id = newParam._id
+    }
+
+    // Update the params
+    for (const param of req.body.params) {
+      const paramToUpdate = await Param.findOne({ _id: param._id })
+      paramToUpdate.name = param.name
+      paramToUpdate.type = param.type
+      paramToUpdate.value = param.value
+      await paramToUpdate.save()
+    }
+
+    // Delete the params that were previously there but are not anymore
+    for (const param of step.params.filter(param => !req.body.params.find(p => p._id === param._id))) {
+      Logger.http('Deleting param ' + param)
+      await Param.deleteOne({ _id: param._id })
+      step.params = step.params.filter(p => p._id !== param._id)
+    }
+
+    await step.save()
+    res.status(200).json(step)
+  } catch (err) {
+    Logger.http(err)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+router.delete('/steps/:stepId', (req, res) => {
+  // Get all the step params
+  Step.findOne({ _id: req.params.stepId })
+    .then((step) => {
+      // Delete all the params
+      Param.deleteMany({ _id: { $in: step.params } })
+        .then(() => {
+          // Delete the step
+          step.deleteOne()
+            .then((step) => res.send(step))
+            .catch((error) => Logger.http(error))
+        })
+        .catch((error) => Logger.http(error))
+    })
+    .catch((error) => Logger.http(error))
+})
+
+router.delete('/all-steps', (req, res) => {
+// Delete all params
+  Param.deleteMany({})
+    .then(() => {
+      // Delete all steps
+      Step.deleteMany({})
+        .then((steps) => res.send(steps))
+        .catch((error) => Logger.http(error))
+    })
+    .catch((error) => Logger.http(error))
 })
 
 module.exports = router
