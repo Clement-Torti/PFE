e83b4a6ff2f4a1f91ebb4e5792a4027c9585e9f2
Clement t <ctorti@edevice.com>
2023-04-26 17:11:09 +0200

    front-end: Adding stepCreationView basic style

--- frontend/src/app/components/step-creation-view/step-creation-view.component.ts
+++ frontend/src/app/components/step-creation-view/step-creation-view.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Step } from 'src/app/models/step';
+import { StepService } from 'src/app/services/step.service';
 
 @Component({
   selector: 'app-step-creation-view',
   templateUrl: './step-creation-view.component.html',
-  styleUrls: ['./step-creation-view.component.css']
+  styleUrls: ['./step-creation-view.component.css'],
 })
 export class StepCreationViewComponent {
+  selectedStep: Step | null = null;
 
+  constructor(private stepService: StepService) {
+    this.stepService.selectedStep$.subscribe((step) => {
+      this.selectedStep = step;
+      console.log('Selected step', step);
+    });
+  }
+
+  onReturn() {
+    console.log('Return');
+  }
+
+  onSave() {
+    console.log('Save');
+  }
+
+  onAddParam() {
+    console.log('Param added');
+  }
+
+  onDeleteStep() {
+    console.log('Step deleted');
+  }
 }
